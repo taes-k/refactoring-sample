31be2eca711ff7f59923df14cc463e7d42a81fe2
taes <taes@wemakeprice.com>
2021-04-28 09:38:37 +0900

    feature: 테스트 코드 작성

---
+++ src/test/java/com/taes/refactoring/StatementTest.java
@@ -0,0 +1,37 @@
+package com.taes.refactoring;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class StatementTest
+{
+    @Test
+    void printCharge_success()
+    {
+        // given
+        Map<String, Play> plays = new HashMap<>();
+        plays.put("hamlet", new Play("Hamlet", "tragedy"));
+        plays.put("as-like", new Play("As You Like It", "comedy"));
+        plays.put("othello", new Play("Othello", "tragedy"));
+
+        Invoice invoice = new Invoice();
+        invoice.setCustomer("BigCo");
+        invoice.setPerformances(
+            Arrays.asList(
+                new Performance("hamlet", 55)
+                , new Performance("as-like", 35)
+                , new Performance("othello", 40)));
+        Statement statement = new Statement();
+
+        // when
+        statement.printCharge(invoice, plays);
+
+        // then
+        Assertions.assertTrue(true);
+
+    }
+}
