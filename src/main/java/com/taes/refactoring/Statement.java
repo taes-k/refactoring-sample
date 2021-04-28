6d2360ead7f041c78e9e97781e7e277efb62e3e7
taes <taes@wemakeprice.com>
2021-04-28 20:32:21 +0900

    feature: html 출력 기능추가

--- src/main/java/com/taes/refactoring/Statement.java
+++ src/main/java/com/taes/refactoring/Statement.java
@@ -1,6 +1,10 @@
 package com.taes.refactoring;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.text.NumberFormat;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
@@ -13,21 +17,66 @@ public class Statement
         this.plays = plays;
     }
 
-    public void printCharge(Invoice invoice)
+    public void printPlainText(Invoice invoice)
+    {
+        Data data = createData(invoice);
+        System.out.println(renderPlainText(data));
+    }
+
+    public void printHtmlText(Invoice invoice)
+    {
+        Data data = createData(invoice);
+        System.out.println(renderHtmlText(data));
+    }
+
+    private Data createData(Invoice invoice)
+    {
+        Data result = new Data();
+
+        result.setCustomer(invoice.getCustomer());
+        result.setPerformances(invoice.getPerformances());
+        result.setTotalAmount(getTotalAmount(invoice));
+        result.setTotalVolumeCredits(getTotalVolumeCredits(invoice));
+
+        return result;
+    }
+
+    private String renderPlainText(Data data)
     {
         StringBuilder result = new StringBuilder();
-        result.append(String.format("청구 내역 (고객명 : %s)\n", invoice.getCustomer()));
+        result.append(String.format("청구 내역 (고객명 : %s)\n", data.getCustomer()));
 
-        for(Performance perf : invoice.getPerformances())
+        for(Performance perf :data.getPerformances())
         {
             // 청구 내역 출력
             result.append(String.format("%s: %d (%d석)\n", playFor(perf).getName(), amountFor(perf), perf.getAudience()));
         }
 
-        result.append(String.format("총액 : %s\n", toUSD(getTotalAmount(invoice))));
-        result.append(String.format("적립 포인트 : %d점\n", getTotalVolumeCredits(invoice)));
+        result.append(String.format("총액 : %s\n", toUSD(data.getTotalAmount())));
+        result.append(String.format("적립 포인트 : %d점\n", data.getTotalVolumeCredits()));
 
-        System.out.println(result);
+        return result.toString();
+    }
+
+    private String renderHtmlText(Data data)
+    {
+        StringBuilder result = new StringBuilder();
+        result.append(String.format("<h1>청구 내역 (고객명 : %s)</h1>\n", data.getCustomer()));
+        result.append("<table>");
+        result.append("<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>");
+
+        for(Performance perf :data.getPerformances())
+        {
+            // 청구 내역 출력
+            result.append(String.format("<tr><td>%s</td><td>%d</td><td>%d석</td></tr>\n"
+                , playFor(perf).getName(), perf.getAudience(), amountFor(perf)));
+        }
+        result.append("</table>");
+
+        result.append(String.format("<p>총액 : <em>%s</em></p>\n", toUSD(data.getTotalAmount())));
+        result.append(String.format("<p>적립 포인트 : <em>%d</em>점</p>\n", data.getTotalVolumeCredits()));
+
+        return result.toString();
     }
 
     private int getTotalAmount(Invoice invoice)
@@ -101,5 +150,15 @@ public class Statement
     private Play playFor(Performance performance)
     {
         return plays.get(performance.getPlayId());
+    }
+
+    @Getter
+    @Setter
+    private class Data
+    {
+        private String customer;
+        private List<Performance> performances;
+        private int totalAmount;
+        private int totalVolumeCredits;
     }
 }
--- src/test/java/com/taes/refactoring/StatementTest.java
+++ src/test/java/com/taes/refactoring/StatementTest.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class StatementTest
 {
     @Test
-    void printCharge_success()
+    void printPlainText_success()
     {
         // given
         Map<String, Play> plays = new HashMap<>();
@@ -28,7 +28,33 @@ public class StatementTest
         Statement statement = new Statement(plays);
 
         // when
-        statement.printCharge(invoice);
+        statement.printPlainText(invoice);
+
+        // then
+        Assertions.assertTrue(true);
+
+    }
+
+    @Test
+    void printHtmlText_success()
+    {
+        // given
+        Map<String, Play> plays = new HashMap<>();
+        plays.put("hamlet", new Play("Hamlet", "tragedy"));
+        plays.put("as-like", new Play("As You Like It", "comedy"));
+        plays.put("othello", new Play("Othello", "tragedy"));
+
+        Invoice invoice = new Invoice();
+        invoice.setCustomer("BigCo");
+        invoice.setPerformances(
+            Arrays.asList(
+                new Performance("hamlet", 55)
+                , new Performance("as-like", 35)
+                , new Performance("othello", 40)));
+        Statement statement = new Statement(plays);
+
+        // when
+        statement.printHtmlText(invoice);
 
         // then
         Assertions.assertTrue(true);
